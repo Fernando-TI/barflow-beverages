534ddcbbd7f2ee8925d18760b13abc35881cbbe7
Fernando Lopes <nando_3008@outlook.com>
2026-02-21 17:40:53 -0300

    Cria DTOs de entrada, atualização e de saída

---
+++ BarFlow.Application/DTOs/CreateBebidaDto.cs
@@ -0,0 +1,11 @@
+﻿using BarFlow.Domain.Enums;
+namespace BarFlow.Application.DTOs
+{
+    public class BebidaDto
+    {
+        public string Nome { get; set; }
+        public TipoBebida Tipo { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
