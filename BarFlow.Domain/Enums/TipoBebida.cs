bcf638c2fa4192346810929fba25403a5fe80077
Fernando Lopes <nando_3008@outlook.com>
2026-02-21 18:10:26 -0300

    Adiciona o tipo Enum com os tipos de bebidas

---
+++ BarFlow.Domain/Enums/TipoBebida.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BarFlow.Domain.Enums;
+
+public enum TipoBebida 
+    {
+        Cerveja = 1,
+        Drink = 2,
+        Destilado = 3,
+        NaoAlcoolica = 4,
+        Refrigerante = 5,
+        Vinho = 6,
+        Agua = 7
+    
+    
+    }
+
+
+
