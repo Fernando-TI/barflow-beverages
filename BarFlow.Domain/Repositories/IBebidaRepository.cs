d66a2c37ebd02603b26194883dfcf5e7b636b71f
Fernando Lopes <nando_3008@outlook.com>
2026-02-21 18:23:19 -0300

    Adiciona o contrato da implementação de bebida

---
+++ BarFlow.Domain/Repositories/IBebidaRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BarFlow.Domain.Entities;
+
+
+namespace BarFlow.Domain.Repositories;
+
+public interface IBebidaRepository
+{
+    IEnumerable<Bebida> ObterTodas();
+    Bebida? ObterPorId(Guid Id);
+    void Adicionar (Bebida bebida);
+
+    void Atualizar(Bebida bebida);
+    void Remover(Guid id);
+}
